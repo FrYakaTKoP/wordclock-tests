5141d12ef45ba05238dee593b1cad4663d917905
FrYakaTKoP <fryakatkop@gmail.com>
2017-04-23 17:31:55 +0200

    s
    Signed-off-by: FrYakaTKoP

--- minimal.js
+++ minimal.js
@@ -11,7 +11,7 @@ ws281x.setIndexMapping(ws281x.indexMapping.alternatingMatrix(12,12));
 setInterval(function() {
     ctx.globalAlpha = 1;
     ctx.fillStyle = '#' + rndi(0xffffff).toString(16);
-    ctx.fillRect(12, 12, 12, 12);
+    ctx.fillRect(rndi(10)-2, rndi(10)-2, rndi(10), rndi(10));
 
     ws281x.render(canvas.toUint32Array());
 }, 100);
